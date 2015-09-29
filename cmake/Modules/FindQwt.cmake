14c98a448ca6b67363f83d347f713a4f790835f1
Paul Cercueil <paul.cercueil@analog.com>
2015-09-29 13:30:59 +0200

    CMake: FindQwt: Find the Qt5 version of QWT instead of the Qt4 version
    Signed-off-by: Paul Cercueil <paul.cercueil@analog.com>

--- cmake/Modules/FindQwt.cmake
+++ cmake/Modules/FindQwt.cmake
@@ -11,10 +11,10 @@ find_path(QWT_INCLUDE_DIRS
   ${CMAKE_INSTALL_PREFIX}/include/qwt
   ${CMAKE_PREFIX_PATH}/include/qwt
   PATHS
-  /usr/local/include/qwt-qt4
+  /usr/local/include/qwt-qt5
   /usr/local/include/qwt
   /usr/include/qwt6
-  /usr/include/qwt-qt4
+  /usr/include/qwt-qt5
   /usr/include/qwt
   /usr/include/qwt5
   /opt/local/include/qwt
@@ -23,7 +23,7 @@ find_path(QWT_INCLUDE_DIRS
 )
 
 find_library (QWT_LIBRARIES
-  NAMES qwt6 qwt6-qt4 qwt qwt-qt4 qwt5 qwtd5
+  NAMES qwt6 qwt6-qt5 qwt qwt-qt5
   HINTS
   ${CMAKE_INSTALL_PREFIX}/lib
   ${CMAKE_INSTALL_PREFIX}/lib64
